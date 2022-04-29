52182a96b5658816645c9b6a67d82ce6533efad2
allan2022 <allan_jing@yahoo.com>
2022-04-29 14:10:05 -0400

    5

--- utils/get_task.py
+++ utils/get_task.py
@@ -3,6 +3,7 @@ import yaml
 
 
 yaml_filename = "../src/all_validation.yaml"
+task_list = None
 
 with open(yaml_filename) as f:
     output = yaml.load(f, Loader=yaml.FullLoader)
@@ -11,3 +12,7 @@ with open(yaml_filename) as f:
     print(task_list)
     for task in task_list:
         print(task)
+
+if not task_list:
+    for task in task_list:
+        print(f'{task_list.index(task)}. {task}')        
