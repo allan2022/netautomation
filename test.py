81c778e01a599a005ef993f7f5babbaa37ad0a87
allan2022 <allan_jing@yahoo.com>
2022-04-29 10:16:02 -0400

    4

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 
-yaml_file_name = "src\all_validation.yaml"
+yaml_file_name = "src/all_validation.yaml"
 with open(yaml_file_name) as f:
     output = yaml.full_load(f)
     print(output)
