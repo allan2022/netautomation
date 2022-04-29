c1c84031dd551e82ee8a8d300043a80534d1b568
allan2022 <allan_jing@yahoo.com>
2022-04-29 13:05:31 -0400

    5

--- network_configuration.py
+++ network_configuration.py
@@ -13,3 +13,21 @@
 4. Network validation
 
 """
+import yaml
+
+yaml_file_name = "src/all_validation.yaml"
+task_list = []
+
+with open(yaml_file_name) as f:
+    output = yaml.load(f, Loader=yaml.FullLoader)
+    task_list = output['tasks'].split()
+
+
+def main(task_list):
+    match task_list:
+        case "aci":
+            print("aci")
+        case "core":
+            print("core")
+        case _:
+            print("not available")
