25edad3fb7d8d9e2b1f6ff27b7f5e27baa784bc2
allan2022 <allan_jing@yahoo.com>
2022-04-29 09:52:52 -0400

    1st

---
+++ network_configuration.py
@@ -0,0 +1,15 @@
+
+"""
+1. Read task
+2. Network validation
+3. Implement change
+    - ACI change
+    - Cisco change
+    - Juniper change
+    - Fortinet change
+    - Palo Alto change
+    - F5 change
+    - IPAM change
+4. Network validation
+
+"""
---
+++ network_validation.py
@@ -0,0 +1,15 @@
+"""
+1. Read task
+2. Setup environment
+3. Network validation
+    - ACI validation
+    - Cisco validation
+    - Juniper validation
+    - Fortinet validation
+    - Palo Alto validation
+    - F5 validation
+    - IPAM validation
+    - Splunk validation
+"""
+
+
