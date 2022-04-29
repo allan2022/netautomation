8e9a3fd1c823ab73f490edcc077edc96861a57ee
allan2022 <allan_jing@yahoo.com>
2022-04-29 13:33:19 -0400

    5

--- network_configuration.py
+++ network_configuration.py
@@ -15,16 +15,22 @@
 """
 import yaml
 
-yaml_file_name = "src/all_validation.yaml"
+# yaml_file_name = "src/all_validation.yaml"
+# tasks = []
+
+# with open(yaml_file_name) as f:
+#     output = yaml.load(f, Loader=yaml.FullLoader)
+#     tasks = output['tasks'].split()
+#     for task in tasks:
+#         print(task)
 
 tasks = "aci"
 
-def run(tasks):
-    match tasks:
-        case "aci":
-            print("aci")
-        case "core":
-            print("core")
-        case _:
-            print("not available")
+match tasks:
+    case "aci":
+        print("aci")
+    case "core":
+        print("core")
+    case _:
+        print("not available")
 
