6db18c55aa68ec1061da6d55c06b5d0dd278fcbc
allan2022 <allan_jing@yahoo.com>
2022-04-29 14:01:22 -0400

    5

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ yaml_file_name = "src/all_validation.yaml"
 
 with open(yaml_file_name) as f:
     output = yaml.load(f, Loader=yaml.BaseLoader)
-    print(output)
     task_list = output['tasks'].split()
     for task in task_list:
         print(task)
---
+++ utils/get_task.py
@@ -0,0 +1,12 @@
+import os
+import yaml
+
+
+yaml_filename = "src/all_validation.yaml"
+
+with open(yaml_filename) as f:
+    output = yaml.load(f, Loader=yaml.FullLoader)
+    print(output)
+    task_list = output['tasks'].split()
+    for task in task_list:
+        print(task)
