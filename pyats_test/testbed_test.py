852f8269ce50b5b65ec44e26e0bb27b10d61410a
allan2022 <allan_jing@yahoo.com>
2022-04-29 22:48:04 -0400

    5

---
+++ pyats_test/testbed_test.py
@@ -0,0 +1,10 @@
+from pyats.topology import Testbed, Device
+
+device_a = Device('A')
+device_b = Device('B')
+device_c = Device('C')
+
+
+testbed_a = Testbed(name = 'firstTestbed', alias = 'yetAnotherTestbed', devices = [device_a, device_b])
+testbed_b = Testbed(name = 'secondTestbed')
+testbed_b.add_device(device_c)
