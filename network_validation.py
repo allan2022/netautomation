c0d6f2f014c2e419b1a167f469952f175b95bf62
allan2022 <allan_jing@yahoo.com>
2022-04-29 20:56:40 -0400

    5

--- network_validation.py
+++ network_validation.py
@@ -11,5 +11,23 @@
     - IPAM validation
     - Splunk validation
 """
+import os
+import yaml
+from utils.get_task import get_task
 
+NETWORK_VALIDATION = os.getcwd() + "src/all_validation.yaml"
+
+task_list, task_select = get_task(NETWORK_VALIDATION)
+
+def main(task_list):
+    match task_list:
+        case "aci":
+            print("aci")
+        case "core":
+            print("core")
+        case _:
+            print("not available")
+
+if __name__ == '__main__':
+    main(task_list)
 
--- utils/get_task.py
+++ utils/get_task.py
@@ -1,20 +1,24 @@
-import os
+from os.path import exists
 import yaml
 
+# yaml_filename = "../src/all_validation.yaml"
+# task_list = None
 
-yaml_filename = "../src/all_validation.yaml"
-task_list = None
+def get_task(yaml_filename = None):
+    task_list = None
+    task_select = None
+    if yaml_filename == None:
+        print(f'{yaml_filename} not found!')
+    elif not exists(yaml_filename):
+        print(f'{yaml_filename} not found!')
+    else:
+        with open(yaml_filename) as f:
+            output = yaml.load(f, Loader=yaml.FullLoader)
+            task_list = output['tasks'].split()
 
-with open(yaml_filename) as f:
-    output = yaml.load(f, Loader=yaml.FullLoader)
-    print(output)
-    task_list = output['tasks'].split()
-    print(task_list)
-    for task in task_list:
-        print(task)
-
-if task_list != None:
-    for task in task_list:
-        print(f'{task_list.index(task) + 1}. {task}')        
-    ans = input("Select a task: ")
-    print(task_list[int(ans)-1])
+        if task_list != None:
+            for item in task_list:
+                print(f'[{task_list.index(item) + 1}]. {item}')        
+            ans = input("Select a task: ")
+            task_select = task_list[int(ans)-1]
+    return task_list, task_select
