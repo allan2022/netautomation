cbfd251cecd64387b2f8b50902f25b2a3cbc6a2d
allan2022 <allan_jing@yahoo.com>
2022-04-29 21:26:04 -0400

    5

--- network_configuration.py
+++ network_configuration.py
@@ -1,39 +1,42 @@
 
 """
 1. Read task
-2. Network validation
-3. Implement change
-    - ACI change
-    - Cisco change
-    - Juniper change
-    - Fortinet change
-    - Palo Alto change
-    - F5 change
-    - IPAM change
-4. Network validation
+2. Network configuration
+3. Implement configuration
+    - ACI configuration
+    - Cisco configuration
+    - Juniper configuration
+    - Fortinet configuration
+    - Palo Alto configuration
+    - F5 configuration
+    - IPAM configuration
+4. Network configuration
 
 """
+from os import getcwd
 import yaml
+from utils.get_task import get_task
 
-# yaml_file_name = "src/all_validation.yaml"
-# tasks = []
+NETWORK_CONFIG = getcwd() + "/src/all_configuration.yaml"
 
-# with open(yaml_file_name) as f:
-#     output = yaml.load(f, Loader=yaml.FullLoader)
-#     tasks = output['tasks'].split()
-#     for task in tasks:
-#         print(task)
+task_list, task_select = get_task(NETWORK_CONFIG)
 
-tasks = ""
-
-def main(tasks):
-    match tasks:
-        case "aci":
-            print("aci")
-        case "core":
-            print("core")
+def main():
+    match task_select:
+        case "core_configuration":
+            print("core_configuration")
+        case "aci_configuration":
+            print("aci_configuration")
+        case "juniper_configuration":
+            print("juniper_configuration")
+        case "f5_configuration":
+            print("f5_configuration")
+        case "fortinet_configuration":
+            print("fortinet_configuration") 
+        case "paloalto_configuration":
+            print("paloalto_configuration")                                               
         case _:
-            print("not available")
+            print("\ntask not available")
 
 if __name__ == '__main__':
-    main(tasks)
+    main()
