368a1abe8dd43cacfea9f5e956ef7e576dbf88f6
allan2022 <allan_jing@yahoo.com>
2022-04-29 21:43:13 -0400

    5

---
+++ core_validation.py
@@ -0,0 +1,8 @@
+
+from pyatstools.pyatslearn import PyatsLearn
+from pyatstools.pyatsdiff import PyatsDiff
+
+
+
+CORE_VALIDATION_CONFIG = getcwd() + '/src/core_validation_config.yaml'
+CORE_DEVICE_LIST = getcwd() + '/src/core-device-list.csv'
