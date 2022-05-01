215a0a6fff58d61b2112d1c4761d26829c6e491d
allan2022 <allan_jing@yahoo.com>
2022-04-30 21:54:16 -0400

    5

---
+++ pyats_test/script_test.py
@@ -0,0 +1,26 @@
+# Example
+# -------
+#
+#   looping on container class instances
+
+from pyats import aetest
+
+# define a container & some inhabitants
+class MyCommonSetup(aetest.CommonSetup):
+    @aetest.subsection
+    def subsection_one(self):
+        self.a = 1
+        print('hello world')
+
+    @aetest.subsection
+    def subsection_two(self):
+        assert self.a == 1
+
+# let's instantiate the class
+common_setup = MyCommonSetup()
+
+# loop through to see what we get:
+for i in common_setup:
+    print(i)
+# subsection_one
+# subsection_two
