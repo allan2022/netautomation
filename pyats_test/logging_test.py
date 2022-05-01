d5b3fcf82f3927e7e59cd35a2a7c2ddaa96572b3
allan2022 <allan_jing@yahoo.com>
2022-04-30 21:44:53 -0400

    5

--- pyats_test/logging_test.py
+++ pyats_test/logging_test.py
@@ -3,5 +3,8 @@ import logging
 
 log = logging.getLogger(__name__)
 
+log.debug('hello debug')
 log.info('hello info')
 log.warning('hello warning')
+log.error('hello error')
+log.critical('hello critical')
