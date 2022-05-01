c1bb1518466f141c0b7e135b09ecc4d79160f534
allan2022 <allan_jing@yahoo.com>
2022-05-01 13:08:31 -0400

    5

--- auto_test/netmiko_test.py
+++ auto_test/netmiko_test.py
@@ -7,17 +7,17 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 
 def connect(hostip):
+    net_connect = ConnectHandler(device_type='cisco_ios', host=hostip, username='cisco', password='cisco', secret='cisco')
+    net_connect.find_prompt()
+    net_connect.enable()
+    accesslists = yaml.load(open('acl.ymlLoader=yaml.SafeLoader'))
+    env = Environment(loader = FileSystemLoader)
+    template = env.get_template('acl.j2') 
+    acl_config = template.render(data=accesslists) 
+    print(acl_config) 
+    print(f"Logged into {hostip} successfully") 
+    output = net_connect.send_config_set(acl_config.split("\n#split method returns a list, that can be used in the send_config_set
 
-net_connect = ConnectHandler(device_type='cisco_ios', host=hostip, username='cisco', password='cisco', secret='cisco')
-net_connect.find_prompt()
-net_connect.enable()
-accesslists = yaml.load(open('acl.ymlLoader=yaml.SafeLoader)
-env = Environment(loader = FileSystemLoader)
-template = env.get_template('acl.j2') 
-acl_config = template.render(data=accesslists) 
-print(acl_config) 
-print(f"Logged into {hostip} successfully") 
-output = net_connect.send_config_set(acl_config.split("\n#split method returns a list, that can be used in the send_config_set
 if __name__ == "__main__": 
-ip = sys.argv[1] 
-connect(ip)
+    ip = sys.argv[1] 
+    connect(ip)
