4de346fc1ce7b28ca8def209dd7172178ebaaa6c
allan2022 <allan_jing@yahoo.com>
2022-05-04 16:16:08 -0400

    r

--- core_validation.py
+++ core_validation.py
@@ -4,7 +4,8 @@ from utils.setup_environment import SetupEnvironment
 # from pyatstools.pyatslearn import PyatsLearn
 # from pyatstools.pyatsdiff import PyatsDiff
 
-CORE_ENV_FILENAME = getcwd() + '/src/core_envrionment.yaml'
 DEVICE_LIST_FILENAME = getcwd() + '/src/device_inventory.csv'
+CORE_CONFIG_FILENAME = getcwd() + '/src/core_envrionment.yaml'
 
 core_validation = SetupEnvironment()
+core_validation.setup_pyats(DEVICE_LIST_FILENAME, CORE_CONFIG_FILENAME)
--- utils/setup_environment.py
+++ utils/setup_environment.py
@@ -1,3 +1,4 @@
+import os
 from utils.load_file import full_load_yaml, full_load_csv
 
 class SetupEnvironment:
@@ -11,8 +12,16 @@ class SetupEnvironment:
 
     def setup_pyats(self, device_filename, config_filename):
         self.device_list = full_load_csv(device_filename)
-        self.command_list = full_load_yaml(config_filename)
-        self.change_number = input("Specify change numebr: ")
+        path = "../testbed"
+        if not os.path.exists(path)
+            os.makedir(path)
+            print(f'created new direcotry {path}')
+        output_file = "../testbed/testbed.yaml"
+        # self.command_list = full_load_yaml(config_filename)
+        # self.change_number = input("Specify change numebr: ")
+        os.system(f'pyats create testbed file --path {self.device_list} --ouput {output_file}')
+        
+        return self
 
 
     def setup_netmiko(self, device_filename, config_filename):
@@ -20,6 +29,7 @@ class SetupEnvironment:
         self.command_list = full_load_yaml(config_filename)
         self.change_number = input("Specify change numebr: ")
 
+        return self
 
 
-
+        task_list = output[task].split()
