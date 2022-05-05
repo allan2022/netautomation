8aec135231623852a0b0a880cf0686712484aaea
allan2022 <allan_jing@yahoo.com>
2022-05-04 21:24:25 -0400

    5

--- core_validation.py
+++ core_validation.py
@@ -5,7 +5,7 @@ from utils.setup_environment import SetupEnvironment
 # from pyatstools.pyatsdiff import PyatsDiff
 
 DEVICE_LIST_FILENAME = getcwd() + '/src/device_inventory.csv'
-CORE_CONFIG_FILENAME = getcwd() + '/src/core_envrionment.yaml'
+CORE_CONFIG_FILENAME = getcwd() + '/src/core_environment.yaml'
 
 core_validation = SetupEnvironment()
 core_validation.setup_pyats(DEVICE_LIST_FILENAME, CORE_CONFIG_FILENAME)
