06562b2ad0917f04818a49ee80efd16eaf4a4ecd
allan2022 <allan_jing@yahoo.com>
2022-05-05 06:59:55 -0400

    5

--- core_validation.py
+++ core_validation.py
@@ -8,14 +8,31 @@ from utils.new_folder import create_folder
 DEVICE_LIST = os.getcwd() + '/src/device_inventory.csv'
 CORE_ENVIRONMENT = os.getcwd() + '/src/core_environment.yaml'
 
-core_validation = SetupEnvironment()
-if core_validation.change_number != "":
-    core_validation.setup_pyats(DEVICE_LIST, CORE_ENVIRONMENT)
 
-    commands = core_validation.command_list
-    testbed = core_validation.testbed_file
+class CoreValidation:
+    def __init__(self):
+        self.commands = ""
+        self.testbed = ""
+    
+    def core_validation(self):
+        core_validation = SetupEnvironment()
+        if core_validation.change_number != "":
+            core_validation.setup_pyats(DEVICE_LIST, CORE_ENVIRONMENT)
 
-    a = create_folder("output")
-    output_folder = create_folder(f'output/{core_validation.change_number}')
+            self.commands = core_validation.command_list
+            self.testbed = core_validation.testbed_file
 
-    os.system(f'pyats learn {commands} --testbed-file {testbed} --output {output_folder}')
+            a = create_folder("output")
+            output_folder = create_folder(f'output/{core_validation.change_number}')
+
+            os.system(f'pyats learn {self.commands} --testbed-file {self.testbed} --output {output_folder}')
+
+def main():
+    cv = CoreValidation()
+    cv.core_validation()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n tast aborted")
