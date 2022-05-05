d9dcd6ac61809be6146006b669b9dd637dc573db
allan2022 <allan_jing@yahoo.com>
2022-05-04 21:46:01 -0400

    5

--- utils/setup_environment.py
+++ utils/setup_environment.py
@@ -11,7 +11,7 @@ class SetupEnvironment:
 
 
     def setup_pyats(self, device_filename, config_filename):
-        self.device_list = full_load_csv(device_filename)['hostname']
+        self.device_list = full_load_csv(device_filename)[0]['hostname']
         self.command_list = full_load_yaml(config_filename)['pyats_learn_features']
         # print(os.getcwd())
         path = "testbed"
