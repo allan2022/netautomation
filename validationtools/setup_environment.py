5fcb6e28e47f842654aacd090f3a7597b1e678b8
allan2022 <allan_jing@yahoo.com>
2022-05-05 17:17:50 -0400

    5

--- validationtools/setup_environment.py
+++ validationtools/setup_environment.py
@@ -21,7 +21,8 @@ class SetupEnvironment:
 
         create_folder("output")
         if self.change_number != "":
-            self.change_folder = create_folder(f'output/{self.change_number}')
+            self.change_folder = os.path.join(os.getcwd(), self.change_number)
+            self.change_folder = create_folder(self.change_folder)
 
 
     def setup_pyats(self, dev_filename, env_filename, test_type):
