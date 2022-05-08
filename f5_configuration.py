a87bfd7dd1c0d64c8358d07cc594f2631748d8fd
allan2022 <allan_jing@yahoo.com>
2022-05-07 21:44:11 -0400

    5

--- f5_configuration.py
+++ f5_configuration.py
@@ -3,7 +3,7 @@ from utils.get_task import get_task
 from envtools.setup_environment import SetupEnvironment
 from netmikotools.netmiko_command import NetmikoCommand
 
-NETMIKO_DEVICE_LIST = os.getcwd() + '/src/f5_configuration_device_inventory.csv'
+F5_DEVICE_LIST = os.getcwd() + '/src/f5_configuration_device_inventory.csv'
 F5_ENVIRONMENT = os.getcwd() + '/src/configuration_environment.yaml'
 
 class F5Configuration:
@@ -16,23 +16,23 @@ class F5Configuration:
         except KeyboardInterrupt:
             pass
 
-    def f5_validation_netmiko(self):
-        netmiko_env = SetupEnvironment(F5_ENVIRONMENT)
-        if netmiko_env.change_number != "":
-            netmiko_env.setup_validation_netmiko(NETMIKO_DEVICE_LIST, F5_ENVIRONMENT, self.task_select)
+    def f5_config(self):
+        f5_env = SetupEnvironment(F5_ENVIRONMENT)
+        if f5_env.change_number != "":
+            f5_env.setup_configuration_netmiko(F5_DEVICE_LIST, F5_ENVIRONMENT, self.task_select)
             
-            devices = netmiko_env.device_list
-            commands = netmiko_env.command_list
-            change_folder = netmiko_env.change_folder
-            snapshot_folder = netmiko_env.snapshot_folder
+            devices = f5_env.device_list
+            commands = f5_env.command_list
+            change_folder = f5_env.change_folder
+            snapshot_folder = f5_env.snapshot_folder
             
-            changenumber = netmiko_env.change_number
-            parser = netmiko_env.parser_folder
+            changenumber = f5_env.change_number
+            parser = f5_env.parser_folder
 
-            print("\n" + "-"*20 + " all devices to be validated " + "-"*20)
+            print("\n" + "-"*20 + " all devices to be configured " + "-"*20)
             for dev in devices:
                 print('{} : {} '.format(dev['device_type'], dev['host'] ))
-            print("-" * (40 + len(" all devices to be validated ")) +"\n")
+            print("-" * (40 + len(" all devices to be configured ")) +"\n")
 
             netmiko_dev = NetmikoCommand()
             netmiko_dev.snapshot(devices, commands, changenumber, snapshot_folder, parser)
@@ -52,8 +52,8 @@ class F5Configuration:
                 pass    
 
 def main():
-    cv = F5Validation()
-    cv.f5_validation_netmiko()
+    cv = F5Configuration()
+    cv.f5_config()
 
 if __name__ == "__main__":
     try:
