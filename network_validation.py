12ca077f9f35e62285cd06264347d922fd1a7182
allan2022 <allan_jing@yahoo.com>
2022-05-19 14:18:42 -0400

    5

--- network_validation.py
+++ network_validation.py
@@ -4,6 +4,7 @@ from core_validation import CoreValidation
 from f5_validation import F5Validation
 from fortinet_validation import FortinetValidation
 from aci_search import ACISearch
+from aci_validation import ACIValidation
 
 CORE_ENVIRONMENT = getcwd() + "/src/core_environment.yaml"
 
@@ -37,8 +38,8 @@ def main():
         task = CoreValidation()
         task.core_validation_netmiko()
     elif task_select == "aci_validation":
-        task = ACISearch()
-        task.aci_search()
+        task = ACIValidation()
+        task.aci_validation()
     elif task_select == "aci_search":
         task = ACISearch()
         task.aci_search()
