6e9506f727f3ecf2f83695a3ec062ea524b82320
allan2022 <allan_jing@yahoo.com>
2022-05-19 13:51:19 -0400

    5

--- acitools/aci_auth.py
+++ acitools/aci_auth.py
@@ -2,11 +2,13 @@ import requests
 
 def aci_auth(auth_url, auth_data):
     requests.packages.urllib3.disable_warnings() 
-    session = requests.session()     
+    session = requests.session()
+    login_res = ""     
 
     try: 
       session.post(auth_url, json=auth_data, verify=False)
     except:
-      print("Login failed")
+      login_res = "Login failed"
+      print(login_res)
 
     return session
