08e098c1285234deed284484b900fc3874107426
allan2022 <allan_jing@yahoo.com>
2022-05-24 14:48:25 -0400

    5

--- core_validation.py
+++ core_validation.py
@@ -29,7 +29,6 @@ class CoreValidation:
             if pyats_env.change_number != "":
                 pyats_env.setup_validation_pyats(PYATS_DEVICE_LIST, CORE_ENVIRONMENT, self.task_select)
 
-                devices = pyats_env.device_list
                 commands = pyats_env.command_list
                 testbed = pyats_env.testbed_file
                 change_folder = pyats_env.change_folder
@@ -37,19 +36,7 @@ class CoreValidation:
 
                 os.system(f'pyats learn {commands} --testbed-file {testbed} --output {snapshot_folder}')
 
-                if self.task_select == "postchange_snapshot_and_diff_prechange_snapshot":
-                    print("#####################  compare postchange with prechange #########################")
-                    before_folder = os.path.join(change_folder, 'prechange_snapshot_0')
-                    os.system(f'pyats diff {before_folder} {snapshot_folder} --output {change_folder}/diff_dir')
-                
-                elif self.task_select == "postchange_snapshot_and_diff_last_postchange_snapshot":
-                    print("#####################  compare postchange with last postchange #########################")
-                    i = int(snapshot_folder.rsplit('_', 1)[-1]) - 1
-                    before_folder = os.path.join(change_folder, ('postchange_snapshot_' + str(i)))
-                    os.system(f'pyats diff {before_folder} {snapshot_folder} --output {change_folder}/diff_dir')
-                
-                else:
-                    pass    
+                diff_folder(change_folder, snapshot_folder, self.task_select)
 
     def core_validation_netmiko(self):
 
